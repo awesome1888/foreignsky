182f5bed1facb5b66bdd5e72225d367a92fee958
Sergey <awesome1888@gmail.com>
2017-04-12 23:29:01 +0200

    started making loader

--- imports/ui/component/header.loader/index.jsx
+++ imports/ui/component/header.loader/index.jsx
@@ -6,13 +6,75 @@ import './style.less';
 
 export default class HeaderLoader extends React.Component {
 
-	// logic here
+	constructor(props)
+	{
+		super(props);
+
+		if(Meteor.isClient)
+		{
+			window.headerLoader = this;
+		}
+
+		this.state = {
+			percent: 0,
+		};
+	}
+
+	componentDidMount()
+	{
+		this.start();
+	}
+
+	start()
+	{
+		this.percent = 0;
+		this.timer = Meteor.setInterval(
+			this.incrementPercent.bind(this),
+			1000
+		);
+	}
+
+	finish()
+	{
+		this.percent = 100;
+	}
+
+	incrementPercent()
+	{
+		this.percent = this.percent + 10;
+	}
+
+	get percent()
+	{
+		return this.state.percent;
+	}
+
+	set percent(value)
+	{
+		value = parseInt(value);
+		if(Number.isNaN(value))
+		{
+			value = 0;
+		}
+
+		if(value < 0 || value > 100)
+		{
+			value = 0;
+		}
+
+		this.setState({
+			percent: value,
+		});
+	}
 
 	render()
 	{
 		return (
 			<div className="header__loading-bar">
-				<div className="header__loading-bar-progress" />
+				<div
+					className="header__loading-bar-progress"
+				    style={{width: this.percent+'%'}}
+				/>
 			</div>
 		);
 	}

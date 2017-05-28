c137011cee3050df64f2d1437cab29ac9b47ed7e
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-05-28 23:27:26 +0200

    deploy fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,14 +2,14 @@
 
 meteor build .;
 scp ../nachberlin.ru.tar.gz root@188.120.254.6:/home/indaberlin/;
-ssh root@188.120.254.6 'systemctl stop nachberlin';
+ssh root@188.120.254.6 'systemctl daemon-reload; systemctl stop nachberlin';
 ssh root@188.120.254.6 'rm -rf /home/indaberlin/bundle/';
 ssh root@188.120.254.6 'tar -xzf /home/indaberlin/nachberlin.ru.tar.gz -C /home/indaberlin/';
 ssh root@188.120.254.6 'cd /home/indaberlin/bundle/programs/server/; /home/indaberlin/.node/current/bin/npm install;'
 ssh root@188.120.254.6 'chown -R indaberlin:indaberlin /home/indaberlin/bundle/';
 ssh root@188.120.254.6 'chown -R indaberlin:indaberlin /home/indaberlin/settings.json';
 ssh root@188.120.254.6 'rm /home/indaberlin/nachberlin.ru.tar.gz';
-ssh root@188.120.254.6 'systemctl start nachberlin';
+ssh root@188.120.254.6 'systemctl daemon-reload; systemctl start nachberlin';
 
 #export PATH="~/.node/current/bin:$PATH";
 #export MONGO_URL='mongodb://localhost:27017/nachberlin';

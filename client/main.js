a3fd0794d0d5eac31ce07a5c8de12de50d8ee22c
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-05-31 23:46:34 +0200

    reichstag

--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
 // Client entry point, imports all client code
 
+window.prerenderReady = false;
+
 import '/imports/startup/client';
 import '/imports/startup/both';
--- imports/lib/prerender.js
+++ imports/lib/prerender.js
@@ -7,7 +7,7 @@ export default class PreRender
     {
         if(Meteor.isClient)
         {
-            window.prerenderReady = false;
+            //window.prerenderReady = false;
         }
     }
 
@@ -18,7 +18,9 @@ export default class PreRender
     {
         if(Meteor.isClient)
         {
-            window.prerenderReady = true;
+            // Meteor.setTimeout(() => {
+            //     window.prerenderReady = true;
+            // }, 10000);
         }
     }
 

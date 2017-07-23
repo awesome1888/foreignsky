24088af3a08f508f3278a1b15e29f3400fdd27c3
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-07-23 12:44:19 +0200

    files moved

--- script/start/app.sh
+++ script/start/app.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #cd $DIR/../../app && meteor npm install
 #cd $DIR/../../app && npm start
 
-cd $DIR/../../ && meteor npm install
-cd $DIR/../../ && npm start
+cd $DIR/../../app/ && meteor npm install
+cd $DIR/../../app/ && npm start

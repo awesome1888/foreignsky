bd99419c0caccaf756f9f4f7ac7caef83aaf526f
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-07-23 13:08:55 +0200

    can run admin

--- script/start/app.sh
+++ script/start/app.sh
@@ -2,8 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#cd $DIR/../../app && meteor npm install
-#cd $DIR/../../app && npm start
-
 cd $DIR/../../app/ && meteor npm install
 cd $DIR/../../app/ && npm start

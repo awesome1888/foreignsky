22f7b61f19852560be0d58d8afdde14a83afbd8e
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-08-29 22:33:56 +0200

    side menu

--- admin/imports/ui/component/layout/component/navigation/navigation.jsx
+++ admin/imports/ui/component/layout/component/navigation/navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react'
+import { Dropdown, Icon, Input, Menu } from 'semantic-ui-react';
 import BaseComponent from '../../../../../lib/base/component/component.jsx';
 // import PropTypes from 'prop-types';
 import entityMap from '../../../../../startup/client/entity-map.js';
@@ -7,24 +7,25 @@ import entityMap from '../../../../../startup/client/entity-map.js';
 
 export default class Navigation extends BaseComponent
 {
-    getItems()
+    // getItems()
+    // {
+    //     const menuItems = [];
+    //
+    //     this.getEntityItems(menuItems);
+    //
+    //     // static
+    //     // todo: inject items here in more sophisticated way
+    //     menuItems.push({
+    //         path: '/task-runner',
+    //         title: 'Task Runner',
+    //     });
+    //
+    //     return menuItems;
+    // }
+
+    getEntityItems()
     {
         const menuItems = [];
-
-        this.makeEntityItems(menuItems);
-
-        // static
-        // todo: inject items here in more sophisticated way
-        menuItems.push({
-            path: '/task-runner',
-            title: 'Task Runner',
-        });
-
-        return menuItems;
-    }
-
-    makeEntityItems(menuItems)
-    {
         entityMap.forEach((item) => {
             const entity = item.entity;
             const listRoute = _.getValue(item, 'route.list');
@@ -36,80 +37,35 @@ export default class Navigation extends BaseComponent
                 });
             }
         });
+
+        return menuItems;
     }
 
     render()
     {
         const { activeItem } = this.state || {}
 
+        // todo: generate this from the structure
         return (
-            <div className="">
-
-                <Menu vertical>
-                    <Menu.Item>
-                        <Menu.Header>Products</Menu.Header>
-
-                        <Menu.Menu>
-                            <Menu.Item name='enterprise' active={activeItem === 'enterprise'} onClick={this.handleItemClick} />
-                            <Menu.Item name='consumer' active={activeItem === 'consumer'} onClick={this.handleItemClick} />
-                        </Menu.Menu>
-                    </Menu.Item>
-
-                    <Menu.Item>
-                        <Menu.Header>CMS Solutions</Menu.Header>
-
-                        <Menu.Menu>
-                            <Menu.Item name='rails' active={activeItem === 'rails'} onClick={this.handleItemClick} />
-                            <Menu.Item name='python' active={activeItem === 'python'} onClick={this.handleItemClick} />
-                            <Menu.Item name='php' active={activeItem === 'php'} onClick={this.handleItemClick} />
-                        </Menu.Menu>
-                    </Menu.Item>
-
-                    <Menu.Item>
-                        <Menu.Header>Hosting</Menu.Header>
-
-                        <Menu.Menu>
-                            <Menu.Item name='shared' active={activeItem === 'shared'} onClick={this.handleItemClick} />
-                            <Menu.Item name='dedicated' active={activeItem === 'dedicated'} onClick={this.handleItemClick} />
-                        </Menu.Menu>
-                    </Menu.Item>
-
-                    <Menu.Item>
-                        <Menu.Header>Support</Menu.Header>
-
-                        <Menu.Menu>
-                            <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
-                                E-mail Support
-                            </Menu.Item>
-
-                            <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
-                                FAQs
-                            </Menu.Item>
-                        </Menu.Menu>
-                    </Menu.Item>
-                </Menu>
-
-
-                {/*{*/}
-                    {/*this.getItems().map((item) => {*/}
-                        {/*return (*/}
-                            {/*<div key={`${item.path} - ${item.title}`}>*/}
-                                {/*<a href={item.path}>{item.title}</a>*/}
-                            {/*</div>*/}
-                        {/*);*/}
-                    {/*})*/}
-                {/*}*/}
-            </div>
+            <Menu vertical className="w_100p">
+                <Dropdown item text='Entity'>
+                    <Dropdown.Menu>
+                        {
+                            this.getEntityItems().map((item) => {
+                                return (
+                                    <Dropdown.Item key={item.path} href={item.path}>
+                                        {item.title}
+                                    </Dropdown.Item>
+                                );
+                            })
+                        }
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Menu.Item name='browse' active={activeItem === 'browse'} href="/task-runner">
+                    <div className="icon_build inline-float_right lh_14px" />
+                    Task Runner
+                </Menu.Item>
+            </Menu>
         );
     }
 }
-
-//<br />
-//<br />
-//<br />
-//<a href="/entity/article">Article</a>
-//    <a href="/entity/article.tag">Article tag</a>
-//<a href="/entity/embed">Embed</a>
-//    <a href="/entity/file">File</a>
-//    <br />
-//    <a href="/task-runner">Task runner</a>

9ffbcb96e37fc82039a4b39e28e46decfa3a4ffc
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-08-31 00:02:51 +0200

    playing with table

--- admin/imports/ui/component/general/list/list.jsx
+++ admin/imports/ui/component/general/list/list.jsx
@@ -6,7 +6,8 @@ import PageNavigation from '../page-navigation/page-navigation.jsx';
 import Row from './component/row/index.jsx';
 import BaseComponent from '../../../../lib/base/component/component.jsx';
 import App from '../../../application.jsx';
-import clone from 'clone';
+
+import { Button, Checkbox, Icon, Table, Menu } from 'semantic-ui-react';
 
 /**
  * The basic component for making lists
@@ -382,6 +383,75 @@ export default class List extends BaseComponent
      */
     render()
     {
+        return (
+            <Table compact celled definition>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell />
+                        <Table.HeaderCell>Name</Table.HeaderCell>
+                        <Table.HeaderCell>Registration Date</Table.HeaderCell>
+                        <Table.HeaderCell>E-mail address</Table.HeaderCell>
+                        <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell collapsing>
+                            <Checkbox slider />
+                        </Table.Cell>
+                        <Table.Cell>John Lilki</Table.Cell>
+                        <Table.Cell>September 14, 2013</Table.Cell>
+                        <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+                        <Table.Cell>No</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell collapsing>
+                            <Checkbox slider />
+                        </Table.Cell>
+                        <Table.Cell>Jamie Harington</Table.Cell>
+                        <Table.Cell>January 11, 2014</Table.Cell>
+                        <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+                        <Table.Cell>Yes</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell collapsing>
+                            <Checkbox slider />
+                        </Table.Cell>
+                        <Table.Cell>Jill Lewis</Table.Cell>
+                        <Table.Cell>May 11, 2014</Table.Cell>
+                        <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
+                        <Table.Cell>Yes</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+
+                <Table.Footer fullWidth>
+                    <Table.Row>
+                        <Table.HeaderCell />
+                        <Table.HeaderCell colSpan='4'>
+                            <Menu floated='right' size='mini' pagination>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='left chevron' />
+                                </Menu.Item>
+                                <Menu.Item as='a'>1</Menu.Item>
+                                <Menu.Item as='a'>2</Menu.Item>
+                                <Menu.Item as='a'>3</Menu.Item>
+                                <Menu.Item as='a'>4</Menu.Item>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='right chevron' />
+                                </Menu.Item>
+                            </Menu>
+
+                            {/*icon*/}
+                            <Button size='small' color='#fff' floated='right'>New article</Button>
+
+                            <Button size='small' color='red'>Remove</Button>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Footer>
+            </Table>
+        );
+
         return (
             <div
                 className={`data-block data-block_transparent ${this.props.className}`}

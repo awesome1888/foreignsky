1094dbb31815432e18e7e3aee28abc5cefea6d68
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-09-08 18:15:58 +0200

    verical scroll block

--- admin/imports/ui/component/general/form/component/renderer/selectbox/index.jsx
+++ admin/imports/ui/component/general/form/component/renderer/selectbox/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import connectField from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
+// import Util from '../../../../../../../lib/util.js';
 
 // https://github.com/vazco/uniforms/blob/master/INTRODUCTION.md#autofield-algorithm
 // https://github.com/vazco/uniforms/blob/master/API.md#connectfield
@@ -13,12 +14,40 @@ import './style.less';
 
 class RendererSelectBox extends RendererGeneric
 {
+    _dropdown = null;
+    _scope = null;
+
     constructor(props)
     {
         super(props);
         this.extendState({
             opened: false,
         });
+
+        this.onDropDownScroll = this.onDropDownScroll.bind(this);
+    }
+
+    onDropDownScroll(e)
+    {
+        // blocking scroll up
+        if (e.deltaY < 0 && this._dropdown.scrollTop <= 0)
+        {
+            e.preventDefault();
+        }
+
+        // blocking scroll down
+        if(e.deltaY > 0)
+        {
+            if (this._dropdown.scrollTop + this.getHeght(this._dropdown) >= this.getHeght(this._scope))
+            {
+                e.preventDefault();
+            }
+        }
+    }
+
+    getHeght(el)
+    {
+        return el.getBoundingClientRect().height;
     }
 
     getEnum()
@@ -80,19 +109,28 @@ class RendererSelectBox extends RendererGeneric
                             }
                             <input type="text" className="selectbox__input" />
                         </div>
-                        <div className="selectbox__dropdown">
-                            {
-                                this.getItems().map((item) => {
-                                    return (
-                                        <label className="selectbox__dropdown-item" key={item.value+item.label}>
-                                            <input type="checkbox" className="selectbox__dropdown-item-checkbox" />
-                                            <div className="selectbox__dropdown-item-text">
-                                                {item.label}
-                                            </div>
-                                        </label>
-                                    );
-                                })
-                            }
+                        <div
+                            className="selectbox__dropdown"
+                            ref={ ref => {this._dropdown = ref; }}
+                            onWheel={this.onDropDownScroll}
+                        >
+                            <div
+                                className="selectbox__dropdown-scope"
+                                ref={ ref => {this._scope = ref; }}
+                            >
+                                {
+                                    this.getItems().map((item) => {
+                                        return (
+                                            <label className="selectbox__dropdown-item" key={item.value+item.label}>
+                                                <input type="checkbox" className="selectbox__dropdown-item-checkbox" />
+                                                <div className="selectbox__dropdown-item-text">
+                                                    {item.label}
+                                                </div>
+                                            </label>
+                                        );
+                                    })
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>

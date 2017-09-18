e07135c9314ff42f0bf978f1dac26b74660b5c4e
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-09-19 00:28:12 +0200

    datepicker ui

--- admin/imports/ui/component/general/form/component/renderer/date/index.jsx
+++ admin/imports/ui/component/general/form/component/renderer/date/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import connectField from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
 
-import { Button, Header, Modal } from 'semantic-ui-react';
+import { Button, Header, Modal, Form } from 'semantic-ui-react';
+import moment from 'moment';
 
 // https://github.com/vazco/uniforms/blob/master/INTRODUCTION.md#autofield-algorithm
 // https://github.com/vazco/uniforms/blob/master/API.md#connectfield
@@ -11,14 +12,19 @@ import { Button, Header, Modal } from 'semantic-ui-react';
 import RendererGeneric from '../generic/index.jsx';
 import Container from '../container/index.jsx';
 
+import './style.less';
+
 class RendererDate extends RendererGeneric
 {
     constructor(props)
     {
         super(props);
         this.extendState({
-            opened: true,
+            opened: false,
         });
+
+        this.onCloseClick = this.onCloseClick.bind(this);
+        this.onOpenClick = this.onOpenClick.bind(this);
     }
 
     onCloseClick()
@@ -28,22 +34,101 @@ class RendererDate extends RendererGeneric
         });
     }
 
-    onSaveClick()
+    onOpenClick()
+    {
+        this.setState({
+            opened: true,
+        });
+    }
+
+    onUpdateClick()
     {
 
     }
 
+    renderMYSelectors()
+    {
+        return (
+            <Form
+                inverted
+                className="margin-b_x2"
+            >
+                <Form.Group>
+                    <Form.Field
+                        inverted
+                        width={10}
+                    >
+                        <select name="" id="">
+                            <option value="1">January</option>
+                            <option value="2">February</option>
+                            <option value="3">March</option>
+                            <option value="4">April</option>
+                        </select>
+                    </Form.Field>
+                    <Form.Field
+                        inverted
+                        width={6}
+                    >
+                        <input type="text" value="1988" />
+                    </Form.Field>
+                </Form.Group>
+            </Form>
+        );
+    }
+
+    renderGridRow(from, to)
+    {
+        const row = [];
+        for(let k = from; k <= to; k++)
+        {
+            row.push(
+                <Button
+                    basic
+                    color='red'
+                    inverted
+                    onClick={this.onCloseClick.bind(this, false)}
+                    className="date-picker__grid-day"
+                    key={k}
+                >
+                    {k}
+                </Button>
+            );
+        }
+
+        return (
+            <div className="date-picker__grid-row-outer">
+                <div className="date-picker__grid-row">
+                    {row}
+                </div>
+            </div>
+        );
+    }
+
     renderGrid()
     {
-        return null;
+        return (
+            <div className="date-picker__grid">
+                {this.renderGridRow(1, 7)}
+                {this.renderGridRow(8, 14)}
+                {this.renderGridRow(15, 21)}
+                {this.renderGridRow(22, 28)}
+            </div>
+        );
     }
 
     renderSelector()
     {
         return (
-            <Modal open={this.state.opened} basic size='small'>
-                <Header icon='date' content={`Pick new ${this.props.attribute.getTitle()}`} />
+            <Modal
+                open={this.state.opened}
+                onClose={this.onCloseClick}
+                basic
+                className="modal_size_mini-custom"
+                closeOnDimmerClick={false}
+            >
+                <Header icon='date' content={`Pick-a-${this.props.attribute.getTitle()}`} />
                 <Modal.Content>
+                    {this.renderMYSelectors()}
                     {this.renderGrid()}
                 </Modal.Content>
                 <Modal.Actions>
@@ -51,38 +136,56 @@ class RendererDate extends RendererGeneric
                         basic
                         color='red'
                         inverted
-                        onClick={this.onCloseClick.bind(this, false)}
+                        onClick={this.onCloseClick}
                     >
                         Close
                     </Button>
                     <Button
                         color='green'
                         inverted
-                        onClick={this.onSaveClick.bind(this, true)}
+                        onClick={this.onUpdateClick.bind(this, true)}
                     >
-                        Save
+                        Update
                     </Button>
                 </Modal.Actions>
             </Modal>
         );
     }
 
+    renderDate()
+    {
+        return (
+            <div className="">
+                {moment(this.getValue()).format('DD MMMM YYYY')}
+            </div>
+        );
+    }
+
     render()
     {
-        console.dir(this.props);
-        
         return (
             <Container
                 errorProps={this.props}
                 {...filterDOMProps(this.props)}
             >
-                <input
-                    type="text"
-                    name={this.getName()}
-                    onChange={this.getOnChange()}
-                    value={this.getValue()}
-                />
-                {this.renderSelector()}
+                <div className="date-picker">
+                    <div
+                        className="date-picker__opener"
+                        onClick={this.onOpenClick}
+                    >
+                        <div className="date-picker__display">
+                            {this.renderDate()}
+                            <div className="date-picker__display-icon" />
+                        </div>
+                    </div>
+                    <input
+                        type="hidden"
+                        name={this.getName()}
+                        onChange={this.getOnChange()}
+                        value={this.getValue()}
+                    />
+                    {this.renderSelector()}
+                </div>
             </Container>
         );
     }

2c2a29ea25ec7e579c02f39ca43300840946fb16
Sergey Gannochenko <gannochenko.sv@gmail.com>
2017-12-21 14:56:07 +0100

    all scripts restored

--- script/test/admin.sh
+++ script/test/admin.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../admin && meteor npm install
-cd $DIR/../../admin && npm run-script test-browser
+cd $DIR/../../admin && npm run-script test
--- script/test/app.sh
+++ script/test/app.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../../app && meteor npm install
-cd $DIR/../../app && npm run-script test-browser
+cd $DIR/../../app && npm run-script test
